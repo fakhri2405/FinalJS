a887db277f91d6483155450ca0f2286cfca9dda8
Гезалов Фахри Эмиль <Gezal_yv57@itstep.edu.az>
2024-12-01 18:24:44 +0400

    fixed return value in 'loadTasks' method

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ class TaskManager {
     constructor() {
       this.tasks = this.loadTasks();
     }
-  
+
     loadTasks() {
       const tasks = JSON.parse(localStorage.getItem('tasks'));
-      return tasks;
+      return tasks ? tasks : [];
     }
-  
+
     saveTasks() {
       localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
-  
+
     addTask(task) {
       this.tasks.push(task);
       this.saveTasks();
     }
-  
+
     removeTask(taskId) {
       this.tasks = this.tasks.filter(task => task.id !== taskId);
       this.saveTasks();
@@ -43,7 +43,7 @@ class TaskManager {
           return this.tasks;
         }
     }
-
+    
     sortTasks(criteria) {
         if (criteria === 'date') {
           return this.tasks.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
